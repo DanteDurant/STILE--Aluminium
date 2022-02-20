0c00e9c2301f26395cd7f1bd290e566683d2c10c
DanteDurant <rathersafe@protonmail.com>
2022-02-20 17:49:15 +0200

    added sliders textboxs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 "use strict";
 
-///////// SLIDER /////////////////////////////////////////////////////////
+// Lazy loading images /////////////////////
+
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
+
+///////// SLIDER ////////////////////////////////////////////////////////////////////
 
 const slider = document.querySelector(".carousel--slider");
 const leftArrow = document.querySelector(".arrow--left__control");
